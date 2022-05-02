9a61aac0e74da74d656654ddc54e1ed344c43c96
mechztcc <dojathan@gmail.com>
2022-05-02 10:58:55 -0300

    feat: add create user route

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,8 +1,13 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 import CreateUserValidator from 'App/Validators/CreateUserValidator'
 
 export default class UsersController {
-    public async store({ request, response }: HttpContextContract) {
-			const payload = request.validate(CreateUserValidator)
-		}
+  public async store({ request, response }: HttpContextContract) {
+    const payload = await request.validate(CreateUserValidator)
+
+    const user = User.create(payload)
+
+    return response.created(user)
+  }
 }
