3a56eb07643d09a370a9ff0b595687e3a56d14c3
mechztcc <dojathan@gmail.com>
2022-05-13 10:31:36 -0300

    feat: successfully auth user

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -5,7 +5,9 @@ export default class AuthController {
   public async store({ request, response, auth }: HttpContextContract) {
     const payload = await request.validate(AuthUserValidator)
 
-    const token = await auth.use('api').authenticate()
+    const token = await auth
+      .use('api')
+      .attempt(payload.email, payload.password, { expiresIn: '2hours' })
     return response.accepted(token)
   }
 }
