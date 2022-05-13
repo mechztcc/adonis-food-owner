4257efbdbe090685282f3a58605769e8f256017f
mechztcc <dojathan@gmail.com>
2022-05-13 09:53:33 -0300

    feat: added hash pass when create a user

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,3 +1,4 @@
+import Hash from '@ioc:Adonis/Core/Hash'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import BadRequestException from 'App/Exceptions/BadRequestException'
 import User from 'App/Models/User'
@@ -7,23 +8,25 @@ export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
     const payload = await request.validate(CreateUserValidator)
 
-
     const userExists = await User.findBy('email', payload.email)
-    if(userExists) {
+    if (userExists) {
       throw new BadRequestException('Email already in use', 409)
     }
+
+    const hashedPass = await Hash.make(payload.password)
+
+    payload.password = hashedPass
 
     const user = await User.create(payload)
 
     return response.created(user)
   }
 
-  public async find({ request, response}: HttpContextContract) {
+  public async find({ request, response }: HttpContextContract) {
     const id = await request.param('id')
 
     const user = await User.findBy('id', id)
 
     return response.accepted(user)
-
   }
 }
