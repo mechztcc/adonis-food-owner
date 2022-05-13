cbf6c65d33c0c7bd4f7c6ddf11044e9c9ca988f4
mechztcc <dojathan@gmail.com>
2022-05-13 10:11:00 -0300

    feat:  AuthUser validator

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,5 +1,8 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import AuthUserValidator from 'App/Validators/AuthUserValidator'
 
 export default class AuthController {
-  public async store({ request, response }: HttpContextContract) {}
+  public async store({ request, response }: HttpContextContract) {
+    const payload = await request.validate(AuthUserValidator)
+  }
 }
---
+++ app/Validators/AuthUserValidator.ts
@@ -0,0 +1,13 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AuthUserValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    email: schema.string({}, [rules.email()]),
+    password: schema.string({}, [rules.minLength(6)]),
+  })
+
+  public messages = {}
+}
