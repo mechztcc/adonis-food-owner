a4141d9365649afe8ef6ec99f837d99808dcd0c4
mechztcc <dojathan@gmail.com>
2022-05-13 10:17:31 -0300

    feat: add basic auth with lucid to project

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -2,7 +2,9 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import AuthUserValidator from 'App/Validators/AuthUserValidator'
 
 export default class AuthController {
-  public async store({ request, response }: HttpContextContract) {
+  public async store({ request, response, auth }: HttpContextContract) {
     const payload = await request.validate(AuthUserValidator)
+
+	const token = await auth
   }
 }
