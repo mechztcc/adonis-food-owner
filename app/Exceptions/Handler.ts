e054c06827043531ab8a343daf970c460b52a8fb
mechztcc <dojathan@gmail.com>
2022-05-15 15:37:33 -0300

    feat: add handle method to 404

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -15,9 +15,21 @@
 
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { Exception } from '@adonisjs/core/build/standalone'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor() {
     super(Logger)
+  }
+
+  public async handle(error: Exception, ctx: HttpContextContract): Promise<any> {
+    if (error.message === 'E_ROW_NOT_FOUND: Row not found') {
+      return ctx.response.status(error.status).send({
+        code: 'BAD_REQUEST',
+        message: 'Resource not found',
+        status: 404,
+      })
+    }
   }
 }
