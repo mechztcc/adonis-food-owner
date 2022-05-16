e66599156ce16945ff1ac5fd246c61d815c1e1ba
mechztcc <dojathan@gmail.com>
2022-05-16 10:45:02 -0300

    feat: add test to create a address to a valide user

--- tests/functional/address/address.spec.ts
+++ tests/functional/address/address.spec.ts
@@ -1,7 +1,24 @@
 import { test } from '@japa/runner'
+import Address from 'App/Models/Address'
+import { UserFactory } from 'Database/factories'
 
 test.group('Address address', () => {
-  test('It should be create a address to valid user', ({ client, assert }) => {
-    
+  test('It should be create a address to valid user', async ({ client, assert }) => {
+    const user = await UserFactory.create()
+
+    const address = {
+      zip: '000-00',
+      street: 'Silent hills',
+      number: '66',
+      city: 'Another Hills',
+      complement: 'Dont be afraid',
+      state: 'Hills',
+      user_id: user.id
+    }
+
+    const response = await client.post('/addresses').json(address)
+
+    const body = await response.body()
+    assert.exists(body.id)
   })
 })
