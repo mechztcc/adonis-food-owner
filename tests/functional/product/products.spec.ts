03e10d0b6da41582fbf149ddfabbd26686b85a38
mechztcc <dojathan@gmail.com>
2022-06-09 12:01:59 -0300

    feat: add test to update a product

--- tests/functional/product/products.spec.ts
+++ tests/functional/product/products.spec.ts
@@ -64,9 +64,36 @@ test.group('Products products', () => {
   })
 
   test('It should be return 404 when try to delete a nonexists product', async ({ client }) => {
-
     const response = await client.delete('products/99')
 
     response.assertBodyContains({ code: 'BAD_REQUEST', message: 'Product not found', status: 404 })
+  })
+
+  test('It should be update a product', async ({ client }) => {
+    const store = await StoreFactory.with('user').create()
+
+    const category = await CategoryFactory.merge({ storeId: store.$attributes.id }).create()
+
+    const data = await client.post('/products').json({
+      name: 'Mussarela P',
+      price: 22.0,
+      description: 'Alot of cheese',
+      available: false,
+      category_id: category.$attributes.id,
+    })
+
+    const body = data.body()
+
+    const response = await client.put(`products/${body.id}`).json({
+      name: 'Mussarela G',
+      price: 32.0,
+      description: 'Alot alot of cheese',
+      available: true,
+      category_id: category.$attributes.id,
+    })
+
+    console.log(response.body());
+
+    response.assertStatus(204)
   })
 })
