a31ac020e513953634fb98eda0833cd73064ebb7
mechztcc <dojathan@gmail.com>
2022-06-09 11:36:51 -0300

    feat: add test to validate when try to create a product with a invalid store

--- tests/functional/product/products.spec.ts
+++ tests/functional/product/products.spec.ts
@@ -25,4 +25,22 @@ test.group('Products products', () => {
       category_id: category.$attributes.id,
     })
   })
+
+  test('It should be return 404 when try to create a product with invalid store', async ({
+    client,
+  }) => {
+    const store = await StoreFactory.with('user').create()
+
+    const category = await CategoryFactory.merge({ storeId: store.$attributes.id }).create()
+
+    const response = await client.post('/products').json({
+      name: 'Mussarela P',
+      price: 22.0,
+      description: 'Alot of cheese',
+      available: false,
+      category_id: 99,
+    })
+
+    response.assertBodyContains({ code: 'BAD_REQUEST', message: 'Category not found', status: 404 })
+  })
 })
